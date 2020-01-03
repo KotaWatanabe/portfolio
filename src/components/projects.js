dcf1335a8e939de92203f122d24ce3ef4e799fb6
Kota Watanabe <kota3316@gmail.com>
2020-01-03 12:49:00 -0800

    projects grid

--- src/components/projects.js
+++ src/components/projects.js
@@ -14,84 +14,80 @@ class Projects extends Component {
     toggleCategories() {
         if(this.state.activeTab === 0){
             return(
-                <div className="projects-grid">
-                <Card className="projects-card" shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                  <CardTitle style={{color: 'fff', height: '30vh', background: `url(${coffee}) center / cover `}}></CardTitle>
-                   <CardText>
-                      Coffee and Speak is matching web service for language teachers and students. <br/> Main programing languages are Ruby on Rails, JavaScript, HTML/CSS,PostgreSQL. <hr/>
-                      3 cool features. <br/>
-                      1. Updating lesson status with the State machine <br/>
-                      2. Shwoing a location using the Google Map API <br/>
-                      3. Pay to the teacher using the Stripe API.
-
-                  </CardText>
-                  
-                  <CardActions border>
+              <div className="grid-wrapper">
+                <div className="box">
+                  <img src={coffee} alt="coffee and speak"/>
+                  <p> Coffee and Speak is matching web service for language teachers and students. <br/> 
+                  Main programing languages are <strong>Ruby on Rails, JavaScript, HTML/CSS, PostgreSQL</strong>.<br/>
+                  3 cool features. <br/>
+                  1. Updating lesson status with the <strong>State machine</strong><br/>
+                  2. Shwoing a location using the <strong>Google Map API </strong><br/>
+                  3. Pay to the teacher using the <strong>Stripe API</strong>.
+                  </p>
+                  <hr/>
+                  <div className="link-buttons">
                     <a href="https://github.com/KotaWatanabe/coffee-and-speak-rails"   target="_blank" rel="noopener noreferrer">
-                      <Button colored>GitHub</Button>
+                      <button>GitHub</button>
                     </a>
                     <a href="https://coffee-and-speak-rails.herokuapp.com/"   target="_blank" rel="noopener noreferrer">
-                      <Button colored>LiveDemo</Button>
+                      <button>LiveDemo</button>
                     </a>
                     <a href="https://www.youtube.com/watch?v=Jq5_KqdxmC4"   target="_blank" rel="noopener noreferrer">
-                      <Button colored>Video</Button>
+                      <button>Video</button>
                     </a>
-                    </CardActions> 
-                    
-
-                  </Card>
-                
-                <Card className="projects-card" shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                  <CardTitle style={{color: 'fff', height: '30vh', background: `url(${quiz}) center / cover `}}></CardTitle>
-                   <CardText>
-                   Quiz Off is an interactive quiz making and taking platform.
-                   Main programing languages are Ruby on Rails, JavaScript, HTML/CSS,PostgreSQL
-                  </CardText>
-                  
-                  <CardActions border>
-                  <a href="https://github.com/KotaWatanabe/quiz-off" target="_blank" rel="noopener noreferrer">
-                    <Button colored>GitHub</Button>
-                  </a>
-                  </CardActions> 
-                </Card>
+                  </div>
                 </div>
+                <div className="box">
+                  <img src={quiz} alt="quiz" className="quiz-image"/>
+                  <p>Quiz Off is an interactive quiz making and taking platform. <br/>
+                  Main programing languages are <strong>Ruby on Rails, JavaScript, HTML/CSS, PostgreSQL</strong></p>
+                  <hr/>
+                  <div className="link-buttons">
+                    <a href="https://github.com/KotaWatanabe/quiz-off"   target="_blank" rel="noopener noreferrer">
+                      <button>GitHub</button>
+                    </a>
+                  </div>
+                </div>
+              </div>
             )
         } else if(this.state.activeTab === 1){
             return (
-              <div className="projects-grid">
-                <Card className="projects-card" shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                  <CardTitle style={{color: 'fff', height: '30vh', background: `url(${celebrityRecognition}) center / cover `}}></CardTitle>
-                    <CardText>
-                      Celebrity-recognition is an app to find the possible name of celebrity from the picture. <br/>
-                      To recognize the celebrity name from picture, I used Clarifai api.
-                      This app is made with <strong>React</strong> for front-end, <strong>Node.js(express, knex)</strong> for back-end and <strong>PostgreSQL</strong> for database.
-                      It's live on Heroku.
-                    </CardText>
-                    <CardActions border>
-                      <a href="https://github.com/KotaWatanabe/Celebrity-Recognition" target="_blank" rel="noopener noreferrer">
-                        <Button colored>GitHub</Button>
-                      </a>
-                      <a href="https://celebrity-recognition.herokuapp.com/"   target="_blank" rel="noopener noreferrer">
-                        <Button colored>LiveDemo</Button>
-                      </a>
-                    </CardActions> 
-                </Card>
-                <Card className="projects-card" shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                  <CardTitle style={{color: 'fff', height: '30vh', background: `url(${footballfetch}) center / cover `}}></CardTitle>
-                  <CardText>
-                    Football-fetch is app for the football enthusiast(like me) to check the updated standings of major European Football leagues. <br/>
-                    This app is made with React and using football-data.org API.
-                  </CardText>
-                  <CardActions border>
-                    <a href="https://github.com/KotaWatanabe/fetch-foot" target="_blank" rel="noopener noreferrer">
-                    <Button colored>GitHub</Button>
-                    </a>
-                    <a href="https://football-fetch.netlify.com"   target="_blank" rel="noopener noreferrer">
-                      <Button colored>LiveDemo</Button>
-                    </a>
-                  </CardActions> 
-                </Card>
-            </div> 
+              <div className="grid-wrapper">
+              <div className="box">
+                <img src={celebrityRecognition} alt="celebrity recognition"/>
+                <p>
+                  Celebrity-recognition is an app to find the name of celebrity from the picture. <br/>
+                  To recognize the celebrity name from picture, I used Clarifai api. <br/>
+                  This app is made with <strong>React</strong> for front-end, <strong>Node.js(express, knex)</strong> for back-end and <strong>PostgreSQL</strong> for database. <br/>
+                  It's live on Heroku.
+                </p>
+                <hr/>
+                <div className="link-buttons">
+                  <a href="https://github.com/KotaWatanabe/Celebrity-Recognition"   target="_blank" rel="noopener noreferrer">
+                    <button>GitHub</button>
+                  </a>
+                  <a href="https://celebrity-recognition.herokuapp.com/"   target="_blank" rel="noopener noreferrer">
+                    <button>LiveDemo</button>
+                  </a>
+                </div>
+              </div>
+              <div className="box">
+                <img src={footballfetch} alt="footballfetch"/>
+                <p>
+                  Football-fetch is app for the football enthusiast(like me) to check the updated standings of major European Football leagues. <br/>
+                  This app is made with <strong>React</strong> and using <strong>football-data.org API</strong>.
+                </p>
+                <hr/>
+                <div className="link-buttons">
+                  <a href="https://github.com/KotaWatanabe/fetch-foot"   target="_blank" rel="noopener noreferrer">
+                    <button>GitHub</button>
+                  </a>
+                  <a href="https://football-fetch.netlify.com"   target="_blank" rel="noopener noreferrer">
+                    <button>LiveDemo</button>
+                  </a>
+                </div>
+              </div>
+            </div>
             )
         }
     }
@@ -103,12 +99,7 @@ class Projects extends Component {
                 <Tab>Ruby on Rails</Tab>
                 <Tab>JS(React)</Tab>
             </Tabs>
-
-              <Grid>
-                <Cell col={12}>
                   <div className="content">{this.toggleCategories()}</div>
-                </Cell>
-              </Grid>
           </div>
           )
     }
